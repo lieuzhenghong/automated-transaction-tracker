d7d1a40d90866fa0244955f1d231385c8e1a3420
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2016-08-16 15:39:23 +0800

    Add temporary add_store.html for testing

--- public/add_store.js
+++ public/add_store.js
@@ -1,14 +1,30 @@
 class Add_Store_Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      store_name: '',
+      _user_id: '',
+    }
+  }
+  handleSubmit() {
+    // Grab the current user from the JSON token?
+  }
   render() {
+    
+
     return(
+        <div id="body">
         <form>
         <label for='store_name'>Store name</label>
         <input type='text' id='store_name'/>
-        
-        <input type='submit' value = 'Create store' />
+
+
+        <input type='submit' value = 'Create store' onClick={this.handleSubmit} />
         </form>
+        </div>
         )
   }
 }
 
-module.exports = Add_Store_Form;
+ReactDOM.render( <Add_Store_Form/>, document.getElementById('content') );
+//module.exports = Add_Store_Form;
